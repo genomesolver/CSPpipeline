666282d753c38b49dc862f08eeae4efb36c62c4b
vinodh98 <vinodh98@gmail.com>
2020-04-17 20:39:15 -0400

    Updated to pull top hits from JSON file

--- README.md
+++ README.md
@@ -2,37 +2,43 @@
 
 ## Steps to run blast.py
 1. Open google colab - https://colab.research.google.com/
-2. Login with your gmail account to enable google drive access. We will use google drive to store and run our python script. All the results will be saved to google drive
-3. Create the following two folders
+2. Login with your gmail account to enable google drive access. We will use google drive to store and run our script.
+3. Create a new notebook
+4. Create the following two folders
     - output
     - tmp
-4. Create an input file (input.txt) with a list of accession numbers
-5. Copy paste the "blast.py" file to colab and save it
-6. Run blast
+5. Create an input file (input.txt) with a list of accession numbers
+6. Copy paste the "blast.py" file to colab and save your notebook
+7. Run blast
 
-## Example output
-### Forward Blast AZA18259.1 (Limit to Bacteria)
-| Percentage | Subject ID |E_Value|
-|--|--|--|
-100.000| AZA18259.1| 0.0|
-94.540| WP_084828638.1*| 0.0|
-91.379| WP_111679482.1| 0.0|
-76.012| WP_155214194.1| 0.0|
-75.434| WP_155214166.1| 0.0|
-**The top hit = WP_084828638.1**
+## Example terminal output
+### Forward Blast QBX23717.1 (Limit to Bacteria)
+|Query Cover % | E_Value | Accession Id | Subject Name|
+|--|--|--|--|
+99.72|0|WP_136022800|Streptococcus pyogenes|
+99.72|0|WP_111679348|Streptococcus dysgalactiae|
+99.72|0|WP_138083809|Streptococcus dysgalactiae|
+99.72|0|WP_136284882|Streptococcus pyogenes|
+99.72|0|WP_032460164|Streptococcus pyogenes|
+99.72|0|WP_110410812|Streptococcus pyogenes|
+99.72|0|WP_136263979|Streptococcus pyogenes|
+99.72|0|WP_136298220|Streptococcus pyogenes|
+96.36|0|WP_136037644|Streptococcus pyogenes|
+96.36|0|WP_136132953|Streptococcus pyogenes|
+**TOP HIT = WP_136022800 Streptococcus pyogenes**
 
 ---------------------------------------
 ### Reverse Blast WP_084828638.1 (Limit to Viruses)
-| Percentage | Subject ID |E_Value|
-|--|--|--|
-95.690|YP_001686804.1|0.0|
-95.690|ARU14331.1|0.0|
-95.115|NP_056680.1|0.0|
-94.540|AZA24404.1|0.0|
-95.402|ARU13760.1|0.0|
-94.253|YP_003344853.1|0.0|
-95.115|ARU14608.1|0.0|
-94.253|AZF92090.1|0.0|
-95.115|AYP29589.1|0.0|
-94.253|AYP30036.1|0.0|
-**The top hit =  YP_001686804.1**
+|Query Cover % | E_Value | Accession Id | Subject Name|
+|--|--|--|--|
+99.72|0|WP_136022800|Streptococcus pyogenes|
+99.72|0|QBX23717|Streptococcus phage Javan146|
+99.72|0|WP_136112598|Streptococcus pyogenes|
+95.84|0|WP_002990034|Streptococcus pyogenes|
+95.84|0|WP_010922218|Streptococcus pyogenes|
+99.72|0|WP_014635509|Streptococcus pyogenes|
+95.84|0|WP_136040266|Streptococcus pyogenes|
+95.84|0|WP_020833526|Streptococcus pyogenes|
+95.84|0|WP_046177708|Streptococcus dysgalactiae|
+99.72|0|WP_136111941|Streptococcus pyogenes|
+**TOP HIT = WP_136022800 Streptococcus pyogenes**
--- blast.py
+++ blast.py
@@ -1,15 +1,15 @@
 import requests
 import re
 import time
-import csv
+import json
 
 # ## Initialize query parameters
 # ## Check the parameter list at https://ncbi.github.io/blast-cloud/dev/api.html
 Forward_Filter = 'ENTREZ_QUERY=txid2[ORGN]'  # Limit to Bacteria
 Reverse_Filter = 'ENTREZ_QUERY=txid10239[ORGN]'  # Limit to Viruses
 url_endpoint = 'https://blast.ncbi.nlm.nih.gov/Blast.cgi?'
-csv_header_row = 'query_id,subject_id,per_identity,alignment_length,mismatches,gap_opens,q_start,q_end,s._start,' \
-                 's._end,evalue,bit_score,sequence\n'
+output_csv_header_row = 'query_id,scientific_name,query_cover_per,evalue,per_identity,accession_id\n'
+
 
 # ## ------- Auxiliary Routines and Classes -----------------
 # A simple routine that extracts an Attribute from a Context given the surrounding marking strings
@@ -18,7 +18,7 @@ def extract_attribute(data, attribute):
     # print("Looking for the attribute:", attribute)
     for line in data.splitlines():
         if attribute in line:
-            #print(line)
+            # print(line)
             attribute_value = re.sub(attribute, "", line)
             attribute_value = re.sub(r"\s", "", attribute_value)
             return attribute_value
@@ -31,7 +31,7 @@ def extract_attribute(data, attribute):
 # ##
 
 def check_request_status(requestid):
-    print("Checking status of RID:", requestid)
+    # print("Checking status of RID:", requestid)
     url_request = 'CMD=Get&FORMAT_OBJECT=SearchInfo&RID=' + requestid
     url_submit = url_endpoint + url_request
     # Submit the request to the BLAST site
@@ -56,15 +56,13 @@ def check_request_status(requestid):
 # ##
 
 
-def blast(protein, query_filter, output_file_name):
+def blast(protein, query_filter, save_json_file_name, save_csv_file_name):
     ###############
     # STEP 1 - Submit the query
     ###############
 
     url_request = 'QUERY=' + protein + '&DATABASE=nr&PROGRAM=blastp&' + query_filter + '&CMD=Put'
     url_submit = url_endpoint + url_request
-    #print(url_submit)
-    print (output_file_name)
 
     # Submit the request to the BLAST site
     Submit_Request = requests.put(url_submit)
@@ -74,94 +72,118 @@ def blast(protein, query_filter, output_file_name):
     f.write(Submit_Request.text)
     f.close()
 
+    # Extract the request id that will be used for next steps
     rid = extract_attribute(Submit_Request.text, "RID = ")
-    #print(rid)
-    rtoe = extract_attribute(Submit_Request.text, "RTOE = ")
-    #print(rtoe)
 
     ###############
-    # STEP 2 - Check the status
+    # STEP 2
     ###############
-    # https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=3BZF6X4G016
-
+    print("Check the status via web-browser:")  # debug msg
+    print("https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=" + rid)  # debug msg
     while True:
         Status, Hits = check_request_status(rid)
         if Status == 'READY' and Hits == 'yes':
-            print("We got some hits yay ! ok will grab the hits file")
+            print("We got some hits, will grab the json hits file")
             break
         elif Status == 'READY' and Hits == 'no':
-            print("There seems to be a problem, we have no hits")
-            print("Check via: https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=", rid)
+            print("Error: We have no hits")  # debug msg
+            print(
+                "Check: https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=" + rid)
             break
         else:
-            print("Not ready, will wait for 60 sec and check again")
-            #print("You can manually check the status @:\n https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=",rid)
+            print("Will wait and check in 60 seconds")  # debug msg
             time.sleep(60)
 
     ###############
-    # STEP 3 - Download the results
+    # STEP 3
     ###############
+    # Download the JSON File to the tmp directory
+    # https://blast.ncbi.nlm.nih.gov/Blast.cgi?RESULTS_FILE=on&FORMAT_TYPE=JSON2_S&FORMAT_OBJECT=Alignment&CMD=Get&RID=3BV6047Z014
 
-    # Download the CSV File
-    # https://blast.ncbi.nlm.nih.gov/Blast.cgi?RESULTS_FILE=on&FORMAT_TYPE=CSV&FORMAT_OBJECT=Alignment&DESCRIPTIONS=10&ALIGNMENT_VIEW=Tabular&CMD=Get&RID=3BV6047Z014
-    url_request = 'RESULTS_FILE=on&FORMAT_TYPE=CSV&FORMAT_OBJECT=Alignment&DESCRIPTIONS=11&ALIGNMENT_VIEW=Tabular&CMD=Get' \
-                  '&RID=' + rid
-    Submit_DownloadRequest = requests.get(url_endpoint + url_request)
-    save_file_handle = open(output_file_name, "w")
-    save_file_handle.write(csv_header_row)
-    save_file_handle.write(Submit_DownloadRequest.text)
-    save_file_handle.close()
+    url_request = 'RESULTS_FILE=on&FORMAT_TYPE=JSON2_S&FORMAT_OBJECT=Alignment&CMD=Get&RID=' + rid
+    Submit_JSONRequest = requests.get(url_endpoint + url_request)
+    save_json_file_handle = open(save_json_file_name, "w")
+    save_json_file_handle.write(Submit_JSONRequest.text)
+    save_json_file_handle.close()
 
-    # print("Downloaded the file")
+    print("Downloaded the JSON hits file")
 
-    ##
+    ###############
     # STEP 4
-    ##
-    # Parse the csv file and write the top 10 results
+    ###############
+    # Parse the JSON file and write the top 10 results to an CSV file in the output folder
 
-    csv_file = open(output_file_name, "r")
-    dict_reader = csv.DictReader(csv_file)
-    i = 0
-    print("########################################")
-    print("Percentage \tSubject ID\tE_Value")
-    print("########################################")
-    for row in dict_reader:
-        if float(row['per_identity']) > 70:
-            # Save the top hit
-            if i == 1:
-                print(row['per_identity'], "\t", row['subject_id'], "\t", row['evalue'], "\t*")
-                i += 1
-                topHit = row['subject_id']
-            else:
-                i += 1
-                print(row['per_identity'], "\t", row['subject_id'], "\t", row['evalue'])
-    print("########################################")
-    print("The top hit = ", topHit)
+    output_csv_file = open(save_csv_file_name, "w")
+    # Add the CSV Header row fields
+    output_csv_file.write(output_csv_header_row)
+
+    with open(save_json_file_name, "r") as read_file:
+        data = json.load(read_file)
+
+    # Extract the query details
+    query_id = data['BlastOutput2'][0]['report']['results']['search']['query_id']
+    query_len = data['BlastOutput2'][0]['report']['results']['search']['query_len']
+
+    print("###################################################")
+    print("Query Cover %\tE_Value\tAccession Id\tSubject Name")
+    print("###################################################")
+
+    # We need only the top 10 hits
+    hit_count = 0
+    for hit in data['BlastOutput2'][0]['report']['results']['search']['hits']:
+        # Extract the fields we need
+        scientific_name = hit['description'][0]['sciname']
+        accession_id = hit['description'][0]['accession']
+        hsps_align_len = hit['hsps'][0]['align_len']
+        hsps_identity = hit['hsps'][0]['identity']
+        hsps_query_from = hit['hsps'][0]['query_from']
+        hsps_query_to = hit['hsps'][0]['query_to']
+        evalue = hit['hsps'][0]['evalue']
+
+        # How to calculate Query Cover %
+        # https://codereview.stackexchange.com/questions/39879/calculate-query-coverage-from-blast-output)
+        query_cover_per = ((hsps_query_to - hsps_query_from) / query_len) * 100
+
+        per_identity = (hsps_identity / hsps_align_len) * 100
+        if (query_cover_per > 70) and (hit_count < 10):
+            if hit_count == 0:
+                topHit = accession_id
+                topHit_scientific_name = scientific_name
+            hit_count += 1
+            print(round(query_cover_per, 2), evalue, accession_id, scientific_name, sep='\t')
+            row = ','.join(
+                (query_id, scientific_name, str(query_cover_per), str(evalue), str(per_identity), accession_id)) + '\n'
+            output_csv_file.write(row)
+    print("###################################################")
+    print("##### TOP HIT = " + topHit, topHit_scientific_name)
+    print("###################################################")
+    output_csv_file.close()
     return topHit
 
 
 # ## ------- End Auxiliary Routines and Classes -----------------
 
-# ##
-# MAIN PROGRAM
-# ##
+# #################
+#   MAIN PROGRAM
+# #################
 
 # Input file with a list of accession numbers
 input_file = open("input.txt", "r")
 for accession_number in input_file:
     print("Working on the Accession Number:", accession_number.rstrip())
     # Forward blast the accession number
-    file_name_slug = accession_number.replace(".1", "")
-    file_name = "output/" + file_name_slug.rstrip() + "-results.csv"
-    topHit = blast(accession_number, Forward_Filter, file_name)
-    # Reverse blast the top hit
+    file_name_slug = (accession_number.replace(".1", ""))
+    save_json_file_name = "tmp/" + file_name_slug.rstrip() + "-results.json"
+    save_csv_file_name = "output/" + file_name_slug.rstrip() + "-results.csv"
+    topHit = blast(accession_number, Forward_Filter, save_json_file_name, save_csv_file_name)
+    # Reverse blast the top hit's accession number
     if topHit:
         print("We have a top hit, running a reverse blast on the Accession Number:", topHit)
         reverse_file_name_slug = topHit.replace(".1", "")
-        file_name = "output/" + file_name_slug.rstrip() + "-reverse-" + reverse_file_name_slug.rstrip() + "-results.csv"
-        blast(topHit, Reverse_Filter, file_name)
+        save_json_file_name = "tmp/" + file_name_slug.rstrip() + "-rev-" + reverse_file_name_slug.rstrip() + "-results.json"
+        save_csv_file_name = "output/" + file_name_slug.rstrip() + "-rev-" + reverse_file_name_slug.rstrip() + "-results.csv"
+        blast(topHit, Reverse_Filter, save_json_file_name, save_csv_file_name)
 input_file.close()
-
 
 ## Troubleshooting (Replace RID with the actual request ID)
 ## Status:
